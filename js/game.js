af90d580a5f85aabb9425c85978016edfa43c5a5
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-25 17:05:47 -0300

    Correção bug Browser Safariindo

--- js/game.js
+++ js/game.js
@@ -30,7 +30,7 @@ let secondCard = '';
 const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
-    if (disabledCards.length === 2) {
+    if (disabledCards.length === 20) {
         clearInterval(this.loop);
         alert(
             `Parabéns ${spanPlayer.innerHTML}! Seu tempo foi de ${timer.innerHTML} segundos!`
