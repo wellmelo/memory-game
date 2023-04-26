c8e9bd39e066d3c39002ee90649afe703c19cc3c
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-26 06:49:00 -0300

    Corrigindo quantidade de Cards a serem exibidos

--- js/game.js
+++ js/game.js
@@ -53,7 +53,7 @@ let secondCard = '';
 const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
-    if (disabledCards.length === 2) {
+    if (disabledCards.length === 20) {
         clearInterval(this.loop);
         // Quando ganha, exibe o modal
         modal.style.display = 'block';
