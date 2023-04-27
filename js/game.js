2960ecc95fe9e20e13bb8eaacb876bf5942411de
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-26 23:55:05 -0300

    Convertendo numeros em minutos teste 02

--- js/game.js
+++ js/game.js
@@ -135,6 +135,8 @@ const startTimer = () => {
     }, 1000);
 };
 
+console.log(currentTime);
+
 window.onload = () => {
     spanPlayer.innerHTML = localStorage.getItem('player');
     nomeplayer.innerHTML = localStorage.getItem('player');
