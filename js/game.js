268d85e2b6413ba20c05f9ac725865756d6e4c7a
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-05-03 21:33:07 -0300

    Quantidade de acertos

--- js/game.js
+++ js/game.js
@@ -97,7 +97,7 @@ let secondCard = '';
 const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
-    if (disabledCards.length === 2) {
+    if (disabledCards.length === 20) {
         clearInterval(this.loop);
 
         // Salva o tempo no localStorage
