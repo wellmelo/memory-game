0eb24b998c648d87455c82a7b2a69997b9b6b889
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-05-06 00:08:07 -0300

    Add new footer in page Index e Game

--- js/game.js
+++ js/game.js
@@ -149,7 +149,7 @@ function pauseClappingAudio() {
 const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
-    if (disabledCards.length === 2) {
+    if (disabledCards.length === 20) {
         clearInterval(this.loop);
 
         // Salva o tempo no localStorage
